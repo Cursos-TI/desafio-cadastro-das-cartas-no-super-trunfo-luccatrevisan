f265853578cfe2adada50d62ed6cb8e1aeeb5310
luccatrevisan <141439341+luccatrevisan@users.noreply.github.com>
2024-10-11 13:53:50 +0000

    atualização desafio aventureiro (densidade e PIB); melhoria na variável Estado (linha 15); melhoria no printf (linha 36).

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-
-int main() {
+int main(){
     char pais[50], estado[50], cidade[50];
     int populacao;
     float area;
     float PIB;
     int pontosTuristicos;
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+
     printf("Bem-vindo ao desafio do SuperTrunfo!\nColoque a sua cidade no jogo digitando abaixo as informações sobre ela! \n");
 
     printf("Digite o nome do país: \n");
     scanf("%s", &pais);
     
-    printf("Digite o nome do estado: \n");
+    printf("Digite o Estado em formato UF (RJ, SP etc): \n");
     scanf("%s", &estado);
 
     printf("Digite o nome da cidade: \n");
@@ -35,15 +29,11 @@ int main() {
 
     printf("Digite a quantidade de pontos turísticos: \n");
     scanf("%d", &pontosTuristicos);
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
 
-    printf("Perfeito! A cidade %s foi registrada no nosso jogo SuperTrunfo! \n", cidade);
-    printf("População: %d \nÁrea: %f \nPIB: %f \nPontos Turísticos: %d", populacao, area, PIB, pontosTuristicos);
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    float densidadePop =  (float) populacao / area;
+    float PIBcapita = (float) PIB / populacao;
 
-    return 0;
+    printf("Perfeito! A cidade %s - %s, %s foi registrada no nosso jogo SuperTrunfo! \n", cidade, estado, pais);
+    printf("População: %d \nÁrea: %.1f \nPIB: %.2f \nPontos Turísticos: %d \n", populacao, area, PIB, pontosTuristicos);
+    printf("Densidade Populacional: %.1f \nPIB per Capita: %.2f", densidadePop, PIBcapita);
 }
